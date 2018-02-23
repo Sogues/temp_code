2c06be4b3c254c9223191a188489642d268eb149
Sogues <mail.caoke@icloud.com>
2018-02-23 12:31:36 +0800

    flask test demo

--- FLASK_PRE/demo2.py
+++ FLASK_PRE/demo2.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import render_template
+from flask import Flask
+from flask import render_template, make_response, abort
+from flask import request
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+app = Flask(__name__)
+
+@app.route('/user/<username>')
+def user_index(username):
+    if username == 'invalid':
+        abort(404)
+    resp = make_response(render_template('user_index.html', username=username))
+    print('---:', resp)
+    resp.set_cookie('username', username)
+    print('---:', resp)
+    return resp
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
