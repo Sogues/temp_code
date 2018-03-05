8b87da3de39d331df8f57b6933c66a59cb5ae192
Sogues <mail.caoke@icloud.com>
2018-03-05 17:38:57 +0800

    complete 3-4

---
+++ louplus_week_3/challenge3_4.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+
+import re
+from datetime import datetime
+
+
+def open_parser(filename):
+    with open(filename) as logfile:
+
+        pattern = (r''
+                '(\d+.\d+.\d+.\d+)\s-\s-\s'  # IP 地址
+                '\[(.+)\]\s'  # 时间
+                '"GET\s(.+)\s\w+/.+"\s'  # 请求路径
+                '(\d+)\s'  # 状态码
+                '(\d+)\s'  # 数据大小
+                '"(.+)"\s'  # 请求头
+                '"(.+)"'  # 客户端信息
+                )
+        parsers = re.findall(pattern, logfile.read())
+
+    return parsers
+
+def main():
+    parsers = open_parser('nginx.log')
+    ip_dict = {}
+    url_dict = {}
+    for item in parsers:
+        if item[0] not in ip_dict.keys():
+            ip_dict[item[0]] = 0
+        if item[2] not in url_dict.keys():
+            url_dict[item[2]] = 0
+        ip_dict[item[0]] += 1
+        if item[3] == '404':
+            url_dict[item[2]] += 1
+    ip_sort = sorted(ip_dict.items(), key=lambda X: X[1], reverse=True)
+    url_sort = sorted(url_dict.items(), key=lambda X: X[1], reverse=True)
+    return ip_sort[0], url_sort[0]
+
+if __name__ == '__main__':
+    ip_dict, url_dict = main()
+    print(ip_dict, url_dict)
