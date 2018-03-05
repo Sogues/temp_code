af66516f4b5daa7431382a806104839e783e59f1
Sogues <mail.caoke@icloud.com>
2018-03-05 18:07:13 +0800

    complete 3-4

--- louplus_week_3/challenge3_4.py
+++ louplus_week_3/challenge3_4.py
@@ -25,16 +25,20 @@ def main():
     ip_dict = {}
     url_dict = {}
     for item in parsers:
-        if item[0] not in ip_dict.keys():
-            ip_dict[item[0]] = 0
+        if "11/Jan/2017" in item[1]:
+            if item[0] not in ip_dict.keys():
+                ip_dict[item[0]] = 0
+            ip_dict[item[0]] += 1
+        if item[3] != '404':
+            continue
         if item[2] not in url_dict.keys():
             url_dict[item[2]] = 0
-        ip_dict[item[0]] += 1
-        if item[3] == '404':
-            url_dict[item[2]] += 1
+        url_dict[item[2]] += 1
     ip_sort = sorted(ip_dict.items(), key=lambda X: X[1], reverse=True)
     url_sort = sorted(url_dict.items(), key=lambda X: X[1], reverse=True)
-    return ip_sort[0], url_sort[0]
+    ip_dict = {ip_sort[0][0]: ip_sort[0][-1]}
+    url_dict = {url_sort[0][0]: url_sort[0][-1]}
+    return ip_dict, url_dict
 
 if __name__ == '__main__':
     ip_dict, url_dict = main()
