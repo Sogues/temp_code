e0bdaad110842dedc7935a9fba4c54294b8bcf6f
Sogues <mail.caoke@icloud.com>
2018-03-22 21:46:07 +0800

    complete challenge23

--- louplus_week_6/datafigure.py
+++ louplus_week_6/datafigure.py
@@ -4,7 +4,16 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 def data_plot():
+    data = pd.read_json('user_study.json')
+    user_data_group = data[['user_id', 'minutes']].groupby('user_id').sum()
     fig = plt.figure()
-    ax = plt.subplots()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(user_data_group)
+    ax.set_title('StudyData')
+    ax.set_xlabel('User ID')
+    ax.set_ylabel('Study Time')
     plt.show()
     return ax
+
+if __name__ == '__main__':
+    data_plot()
