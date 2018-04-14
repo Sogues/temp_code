c49888deb8d79d883dc45b9f81dc0bad95e28ce0
Sogues <mail.caoke@icloud.com>
2018-04-14 11:35:41 +0800

    challenge 30

---
+++ louplus_week_8/lab3/simpledu/config.py
@@ -0,0 +1,22 @@
+class BaseConfig(object):
+    SECRET_KEY = 'makesure to set a very secret key'
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://sogues:123@180.76.239.229:3306/hhh_week_7?charset=utf8'
+
+
+class ProductionConfig(BaseConfig):
+    pass
+
+
+class TestingConfig(BaseConfig):
+    pass
+
+
+configs = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
+
